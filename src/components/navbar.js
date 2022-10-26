467fcb43782ede233547649e1d1e631d785f7913
ak-yas <lolesp295@gmail.com>
2022-10-26 23:54:51 +0100

    added product card and basket modal

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,31 @@
-import {Button, Container, Navbar, Modal} from 'react-bootstrap';
+import { useState } from 'react'
+import { Button, Container, Navbar, Modal } from 'react-bootstrap'
 
-const NavbarContainer = () =>  {
+const NavbarContainer = () => {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   return (
-    <Navbar expand="sm">
+    <>
+      <Navbar expand="sm">
         <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className='justify-content-end'>
-            <Button>Cart 0 Items</Button>
+        <Navbar.Collapse className="justify-content-end">
+          <Button onClick={handleShow}>Cart 0 Items</Button>
         </Navbar.Collapse>
-    </Navbar>
+      </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>This is the modal body</h1>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
 
-export default  NavbarContainer
+export default NavbarContainer
--- src/pages/store.js
+++ src/pages/store.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import ProductCard from '../components/productCard'
 import { productsArray } from '../productsStore'
 
 function Store() {
   return (
     <>
-      <h1 align="center" className='p-3'>Welcome to the Store</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the Store
+      </h1>
       <Row xs={1} md={3} className="g-4">
         {productsArray.map((product, idx) => (
           <Col align="center" key={idx}>
-            <h1>{product.title}</h1>
+            <ProductCard product={product} />
           </Col>
         ))}
       </Row>
