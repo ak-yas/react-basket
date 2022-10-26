9a16123c14a9bbb59380ddefb8bf8430f69e52b1
ak-yas <lolesp295@gmail.com>
2022-10-26 20:57:20 +0100

    added the setup for basic components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavbarContainer from './components/navbar'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Store from './pages/store'
+import Success from './pages/success'
+import Cancel from './pages/cancel'
 
 function App() {
   return (
     <Container>
       <NavbarContainer></NavbarContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   )
 }
---
+++ src/pages/store.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+function Store() {
+  return (
+    <>
+      <h1>Welcome to the Store</h1>
+      <Row xs={1} md={3} className="g-4">
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Store
