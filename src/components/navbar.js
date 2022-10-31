2bd7d06e91fcdfc59be26b474f36792d20e2d885
ak-yas <lolesp295@gmail.com>
2022-10-31 22:37:19 +0100

    added stripe server for checkout

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,6 +14,24 @@ const NavbarContainer = () => {
     0
   )
 
+  const checkout = async () => {
+    await fetch('http://localhost:4000/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ items: cart.items }),
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        if (response.url) {
+          window.location.assign(response.url) // Forwarding user to Stripe
+        }
+      })
+  }
+
   return (
     <>
       <Navbar expand="sm">
@@ -42,7 +60,9 @@ const NavbarContainer = () => {
 
               <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
 
-              <Button variant="success">Purchase items!</Button>
+              <Button variant="success" onClick={checkout}>
+                Purchase items!
+              </Button>
             </>
           ) : (
             <h1>There are no items in your cart!</h1>
--- src/productsStore.js
+++ src/productsStore.js
@@ -1,16 +1,16 @@
 const productsArray = [
   {
-    id: '1',
+    id: 'price_1Lz3lIKEEIxhRDJ8gj9zg5B1',
     title: 'Guitar',
     price: 84.99,
   },
   {
-    id: '2',
+    id: 'price_1Lz3mNKEEIxhRDJ8DskBEWMs',
     title: 'Pen',
     price: 9.99,
   },
   {
-    id: '3',
+    id: 'price_1Lz3n7KEEIxhRDJ8Nvp1ButM',
     title: 'Watch',
     price: 69.99,
   },
